0c46029be8538b500535add79184fb329f5ef2d1
Psychogrinder <noirart@gmail.com>
2018-08-08 23:51:11 +0300

    Fix page layout; Add filters

--- script.js
+++ script.js
@@ -21,16 +21,46 @@ var createCards = function(obj){
     nameContainer.className = "name-container";
     infoContainer.appendChild(nameContainer);
 
+    var ageContainer = document.createElement("div");
+    ageContainer.className = "age-container";
+    infoContainer.appendChild(ageContainer);
+
     var emailContainer = document.createElement("div");
-    emailContainer.className = "name";
+    emailContainer.className = "email";
     infoContainer.appendChild(emailContainer);
 
     nameContainer.innerHTML = results[i].name.title + ". " + results[i].name.first + " " + results[i].name.last;
+    ageContainer.innerHTML = results[i].dob.age;
     emailContainer.innerHTML = results[i].email;
   }
 };
 
+var ageFilter = function(isOlder){
+  var allCards = document.querySelectorAll(".card");
+  for (var i = 0; i < allCards.length; i++) {
+    var card = allCards[i];
+    card.classList.remove("hidden");
+    var ageContainer = card.querySelector(".age-container");
+    var age = parseInt(ageContainer.innerHTML);
+    if (isOlder) {
+      if (age <= 40) {
+        card.classList.add("hidden");
+      }
+    } else {
+      if (age > 40) {
+        card.classList.add("hidden");
+      }
+    }
+  }
+};
 
+var showAll = function () {
+  var allCards = document.querySelectorAll(".card");
+  for (var i = 0; i < allCards.length; i++) {
+    var card = allCards[i];
+    card.classList.remove("hidden");
+  }
+};
 
 $( document ).ready(function() {
   var jsonData;
@@ -41,10 +71,6 @@ $( document ).ready(function() {
     success: function(data) {
       jsonData = data;
       createCards(jsonData);
-      console.log(jsonData.results);
     }
   });
-
-
-
 });
