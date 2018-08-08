c7e8a012c6fb9d92074c7f67b6dfd564a708168a
Psychogrinder <noirart@gmail.com>
2018-08-08 14:28:07 +0300

    Add function in script

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
+var createCards = function(obj){
+  var cardInfo = document.getElementsByClassName("container")[0];
+  var results = obj.results;
+  for (var i = 0; i < results.length; i++) {
+    var card = document.createElement("div");
+    card.innerHTML = results[i].name;
+    cardInfo.appendChild(card);
+    var span = document.createElement("span");
+    span.innerHTML = results[i].name;
+    cardInfo.appendChild(span);
+  }
+};
+
 $( document ).ready(function() {
+  var jsonData;
+
   $.ajax({
     url: 'https://randomuser.me/api/?results=100',
     dataType: 'json',
     success: function(data) {
-      console.log(data);
+      jsonData = data;
+      createCards(jsonData);
+      console.log(jsonData);
     }
   });
 
 
 
-
-
-
-
-
 });
-
-
-
-// var getJson = function(){
-//
-// };
